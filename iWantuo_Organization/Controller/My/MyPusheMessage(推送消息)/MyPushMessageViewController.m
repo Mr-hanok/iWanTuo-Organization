1309cebe94f4f842b2196596f3485670912e17c0
wuyue <1120884214@qq.com>
2016-02-25 10:31:23 +0800

    广场页面微调

--- iWantuo_Organization/Controller/My/MyPusheMessage(推送消息)/MyPushMessageViewController.m
+++ iWantuo_Organization/Controller/My/MyPusheMessage(推送消息)/MyPushMessageViewController.m
@@ -36,7 +36,6 @@
     [super viewWillLayoutSubviews];
     self.messageTV.layer.borderWidth = 1.f;
     self.messageTV.layer.borderColor = [UIColor lightGrayColor].CGColor;
-//    self.messageTV.returnKeyType = UIReturnKeyDone;
     
 }
 #pragma mark - 协议名
--- iWantuo_Organization/Controller/My/MyViewController.m
+++ iWantuo_Organization/Controller/My/MyViewController.m
@@ -200,10 +200,10 @@
  */
 - (IBAction)Logout:(UIButton *)sender {
     
-    if ([[AccountManager sharedInstance].account.isLogin isEqualToString:@"no"] ||[[ValueUtils stringFromObject:[AccountManager sharedInstance].account.isLogin] isEqualToString:@""]) {
-        [HUDManager showWarningWithText:@"尚未登录"];
-        return;
-    }
+//    if ([[AccountManager sharedInstance].account.isLogin isEqualToString:@"no"] ||[[ValueUtils stringFromObject:[AccountManager sharedInstance].account.isLogin] isEqualToString:@""]) {
+//        [HUDManager showWarningWithText:@"尚未登录"];
+//        return;
+//    }
     [AlertViewManager showAlertViewMessage:@"确定要退出登录吗" handlerBlock:^{
         self.apiLoginOut = [[ApiLoginOutRequest alloc]initWithDelegate:self];
         [self.apiLoginOut setApiParams];
